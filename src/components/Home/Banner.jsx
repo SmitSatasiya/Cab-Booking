400187dbec0d91f98053ea64b87a969f42136a02
SmitSatasiya10 <163808557+SmitSatasiya10@users.noreply.github.com>
2025-01-10 23:39:58 +0530

    Banner Changes

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -1,19 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import { Mousewheel, Keyboard } from 'swiper/modules';
 
-import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules';
 export const Banner = () => {
-    const getDate = () => {
-        const date = new Date().toUTCString().slice(5, 16);
+    const [currentDate, setCurrentDate] = useState('');
+    const [formattedDate, setFormattedDate] = useState('');
+    const [currentTime, setCurrentTime] = useState('');
 
-        return (date)
-    }
+    // Initialize with current date and time
+    useEffect(() => {
+        const now = new Date();
 
-    return (<>
-        <section className='section banner-container'>
+        const isoDate = now.toISOString().slice(0, 10); // For date input (YYYY-MM-DD)
+        const displayDate = new Intl.DateTimeFormat('en-US', { 
+            month: 'long', 
+            day: 'numeric', 
+            year: 'numeric' 
+        }).format(now); // For display (March 10, 2025)
+
+        const formattedTime = now.toTimeString().slice(0, 5); // Format as HH:MM
+
+        setCurrentDate(isoDate);
+        setFormattedDate(displayDate);
+        setCurrentTime(formattedTime);
+    }, []);
+
+    // Handlers for user input
+    const handleDateChange = (e) => {
+        setCurrentDate(e.target.value); // Update date value
+    };
+
+    const handleTimeChange = (e) => {
+        setCurrentTime(e.target.value); // Update time value
+    };
+
+    return (
+        <section className="section banner-container">
             <Swiper
                 cssMode={true}
                 mousewheel={true}
@@ -21,105 +46,66 @@ export const Banner = () => {
                 modules={[Mousewheel, Keyboard]}
                 className="banner-swiper"
             >
-                <SwiperSlide className='banner-slide'>
+                <SwiperSlide className="banner-slide">
                     <div className="img-box">
-
-                        <img className='banner-img' src="/images/background/banner.png" alt="" />
+                        <img
+                            className="banner-img"
+                            src="/images/background/banner.png"
+                            alt=""
+                        />
                     </div>
                     <div className="container d-flex">
                         <div className="banner-content">
-
                             <div className="title">
                                 <p>Embark on an Unforgettable Journey</p>
-                                <h1 >Discover the <br /> World with Our Expert Guides</h1>
+                                <h1>Discover the <br /> World with Our Expert Guides</h1>
                             </div>
-
                         </div>
                     </div>
                 </SwiperSlide>
-
-                <SwiperSlide className='banner-slide'>
-
-                    <div className="img-box">
-
-                        <img className='banner-img' src="/images/background/banner2.png" alt="" />
-                    </div>
-                    <div className="container banner-content d-flex">
-                        <div className="title">
-                            <h1 >Find Your <br /> Perfect Escape</h1>
-                        </div>
-                    </div>
-
-                </SwiperSlide>
-
-                <SwiperSlide className='banner-slide'>
-
-                    <div className="img-box">
-
-                        <img className='banner-img' src="/images/background/banner3.png" alt="" />
-                    </div>
-                    <div className="container banner-content d-flex">
-                        <div className="title">
-                            <h1 >Travel in <br /> Style and Comfort</h1>
-                        </div>
-                    </div>
-
-                </SwiperSlide>
-                <SwiperSlide className='banner-slide'>
-
-                    <div className="img-box">
-
-                        <img className='banner-img' src="/images/background/banner4.png" alt="" />
-                    </div>
-                    <div className="container banner-content d-flex">
-                        <div className="title">
-                            <h1>Relex in <br />the World's Most Beautiful Locations</h1>
-                        </div>
-                    </div>
-
-                </SwiperSlide>
-                <SwiperSlide className='banner-slide'>
-
-                    <div className="img-box">
-
-                        <img className='banner-img' src="/images/background/banner5.png" alt="" />
-                    </div>
-                    <div className="container banner-content d-flex">
-                        <div className="title">
-                            <h1>Culinary Tours <br />to Excite Your Palate</h1>
-                        </div>
-                    </div>
-
-                </SwiperSlide>
+                {/* Other slides */}
                 <div className="form">
-                    <form action="">
+                    <form>
+                        {/* Editable Date Input */}
                         <div className="input-box">
                             <div className="img-box">
                                 <img src="/images/icons/calendar.svg" alt="" />
                             </div>
                             <div className="info">
-                                <label htmlFor="date" >Date</label>
-                                <input type="date" onLoad={getDate} />
+                                <label htmlFor="date">Date</label>
+                                <input
+                                    type="date"
+                                    id="date"
+                                    value={currentDate} // Controlled input in YYYY-MM-DD format
+                                    onChange={handleDateChange} // Handle manual updates
+                                />
                             </div>
                         </div>
 
+                        {/* Editable Time Input */}
                         <div className="input-box">
                             <div className="img-box">
                                 <img src="/images/icons/time.svg" alt="" />
                             </div>
                             <div className="info">
                                 <label htmlFor="time">Time</label>
-                                <input type="time" />
+                                <input
+                                    type="time"
+                                    id="time"
+                                    value={currentTime} // Controlled input
+                                    onChange={handleTimeChange} // Handle manual updates
+                                />
                             </div>
                         </div>
 
+                        {/* Other inputs */}
                         <div className="input-box">
                             <div className="img-box">
                                 <img src="/images/icons/from.svg" alt="" />
                             </div>
                             <div className="info">
                                 <label htmlFor="from">From</label>
-                                <input type="text" placeholder='From' />
+                                <input type="text" placeholder="From" />
                             </div>
                         </div>
 
@@ -129,50 +115,20 @@ export const Banner = () => {
                             </div>
                             <div className="info">
                                 <label htmlFor="to">To</label>
-                                <input type="text" placeholder='To' />
+                                <input type="text" placeholder="To" />
                             </div>
                         </div>
-                        <button className='btn btn-search' >
-                            <img src="/images/icons/search.svg" alt="search" loading='lazy' />
+                        <button className="btn btn-search">
+                            <img
+                                src="/images/icons/search.svg"
+                                alt="search"
+                                loading="lazy"
+                            />
                             Search
                         </button>
                     </form>
                 </div>
             </Swiper>
-
-            {/* <div className="bottom-right">
-                            <form action="" className='d-flex'>
-                                <div className="input d-flex">
-                                    <img className='location' height={30} width={20} src="/images/icons/location.png" alt="" />
-                                    <input type="text" placeholder='From' />
-                                </div>
-                                <div className="input d-flex">
-                                    <img className='location' height={30} width={20} src="/images/icons/location.png" alt="" />
-                                    <input type="text" placeholder='To' />
-                                </div>
-                                <div className="input d-flex">
-                                    <img height={22} width={22} src="/images/icons/call.png" alt="" />
-                                    <input type="text" placeholder='Phone Number' />
-
-                                </div>
-                                <div className="input d-flex">
-                                    <img height={25} width={25} src="/images/icons/time.png" alt="" />
-                                    <input type="time" placeholder='Time' />
-                                </div>
-
-                                <div className="input d-flex">
-                                    <img height={25} width={25} src="/images/icons/car.png" alt="" />
-                                    <input type="text" placeholder='Car Type' />
-                                </div>
-                                <div className="input d-flex">
-                                    <img className='payment' height={20} width={20} src="/images/icons/payment.png" alt="" />
-                                    <input type="text" placeholder='Payment' />
-                                </div>
-                                <button className='btn book-btn'> Order taxi now</button>
-                            </form>
-                        </div> */}
-
         </section>
-
-    </>)
-}
+    );
+};
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import '../../components/Home/Home.css'
 import { Banner } from '../../components/Home/Banner'
-import { CarClasses2 } from '../../components/Home/CarClasses2'
 import { Work } from '../../components/Home/Work'
 import { Service } from '../../components/Home/Services'
 import { AboutComponet } from '../../components/Home/AboutComponet'
@@ -13,7 +12,6 @@ export const Home = () => {
         <>
             <Banner />
             <Partner />
-            <CarClasses2 />
             <Work />
             <AboutComponet />
             <Service />
